faee8e8d9d6a93d2d46a9390ed8d10afbaca272c
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-20 18:11:48 +0200

    not workin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
@@ -40,3 +40,58 @@ const App: React.FC = () => (
 );
 
 export default App;
+
+// import { Redirect, Route } from "react-router-dom";
+// import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+// import { IonReactRouter } from "@ionic/react-router";
+
+// import "@ionic/react/css/ionic.bundle.css";
+
+// /* Theme variables */
+// import "./theme/variables.css";
+// import { LoginPage } from "./pages/Login";
+// import { AccountPage } from "./pages/Account";
+// import { useEffect, useState } from "react";
+// import { Session } from "@supabase/supabase-js";
+// import { supabase } from "./lib/SupabaseConfig";
+
+// setupIonicReact();
+
+// const App: React.FC = () => {
+//   const [session, setSession] = useState<Session | null>(null);
+
+//   useEffect(() => {
+//     supabase.auth.getSession().then(({ data: { session } }) => {
+//       setSession(session);
+//     });
+
+//     supabase.auth.onAuthStateChange((_event, session) => {
+//       setSession(session);
+//     });
+//   }, []);
+
+//   return (
+//     <IonApp>
+//       <IonReactRouter>
+//         <IonRouterOutlet>
+//           <Route
+//             exact
+//             path="/"
+//             render={() => {
+//               return session && session.user ? (
+//                 <Redirect to="/account" />
+//               ) : (
+//                 <LoginPage />
+//               );
+//             }}
+//           />
+//           <Route exact path="/account">
+//             <AccountPage key={session!.user.id} session={session!} />
+//           </Route>
+//         </IonRouterOutlet>
+//       </IonReactRouter>
+//     </IonApp>
+//   );
+// };
+
+// export default App;
---
+++ src/pages/Account.tsx
@@ -0,0 +1,143 @@
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonLoading,
+  useIonToast,
+  useIonRouter,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/SupabaseConfig";
+import { Session } from "@supabase/supabase-js";
+
+export function AccountPage({ session }: { session: Session }) {
+  const [showLoading, hideLoading] = useIonLoading();
+  const [showToast] = useIonToast();
+  const router = useIonRouter();
+  const [profile, setProfile] = useState({
+    username: "",
+    website: "",
+    avatar_url: "",
+  });
+  useEffect(() => {
+    if (session) getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    await showLoading();
+    try {
+      if (!session?.user) throw new Error("No user on the session");
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", session?.user!.id)
+        .single();
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setProfile({
+          username: data.username,
+          website: data.website,
+          avatar_url: data.avatar_url,
+        });
+      }
+    } catch (error: any) {
+      showToast({ message: error.message, duration: 5000 });
+    } finally {
+      await hideLoading();
+    }
+  }
+  async function signOut() {
+    await supabase.auth.signOut();
+    router.push("/", "forward", "replace");
+  }
+  async function updateProfile(e?: any, avatar_url: string = "") {
+    e?.preventDefault();
+    await showLoading();
+
+    try {
+      if (!session?.user) throw new Error("No user on the session!");
+
+      const updates = {
+        id: session?.user.id,
+        ...profile,
+        avatar_url: avatar_url,
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+
+      if (error) {
+        throw error;
+      }
+    } catch (error: any) {
+      showToast({ message: error.message, duration: 5000 });
+    } finally {
+      await hideLoading();
+    }
+  }
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Account</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <form onSubmit={updateProfile}>
+          <IonItem>
+            <IonLabel>
+              <p>Email</p>
+              <p>{session?.user?.email}</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel position="stacked">Name</IonLabel>
+            <IonInput
+              type="text"
+              name="username"
+              value={profile.username}
+              onIonChange={(e) =>
+                setProfile({ ...profile, username: e.detail.value ?? "" })
+              }
+            ></IonInput>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel position="stacked">Website</IonLabel>
+            <IonInput
+              type="url"
+              name="website"
+              value={profile.website}
+              onIonChange={(e) =>
+                setProfile({ ...profile, website: e.detail.value ?? "" })
+              }
+            ></IonInput>
+          </IonItem>
+          <div className="ion-text-center">
+            <IonButton fill="clear" type="submit">
+              Update Profile
+            </IonButton>
+          </div>
+        </form>
+
+        <div className="ion-text-center">
+          <IonButton fill="clear" onClick={signOut}>
+            Log Out
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+}
