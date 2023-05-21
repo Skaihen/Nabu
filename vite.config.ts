cba2620ed2cfd5e31cc422800f33bca7f6b0c170
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-21 15:06:43 +0200

    svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
+  plugins: [svelte()],
 })
