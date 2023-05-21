d8979e490b70702d7d3ff655f4d1947695bd9de5
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-21 17:27:54 +0200

    capacitor

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
+    }),
+  },
+};
