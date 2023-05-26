6eaffec2154139ff8e86a7441cad29a892f58711
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-26 19:41:20 +0200

    Update ListItem.svelte

--- src/components/ListItem.svelte
+++ src/components/ListItem.svelte
@@ -6,25 +6,24 @@
 
 <button
   on:click={onCardClick}
-  class="btn btn-block card h-auto mb-4 shadow-xl relative inline-flex items-center justify-start overflow-hidden font-medium transition-all hover:bg-red-500 bg-red-500 rounded-md group"
+  class:isCompletedElement={isCompleted}
+  class="btn btn-block card h-auto mb-4 shadow-xl rounded-md relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-red-500 group
+  element"
 >
   <span
-    class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4"
+    class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-300 delay-100 ease-in-out bg-accent-focus rounded-sm group-hover:-mr-4 group-hover:-mt-4"
   >
     <span
-      class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"
+      class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-accent-content"
     />
   </span>
   <span
-    class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-md group-hover:mb-12 group-hover:translate-x-0"
-  />
-  <span
     class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
   >
-    <div class="card-body px-4 py-0 w-full">
-      <p class="card-actions justify-end text-lg">Valor: X</p>
+    <div class="card-body px-4 py-3 w-full">
+      <p class="card-actions justify-end">Valor: X</p>
       <h1 class:isCompleted class="card-title">{text}</h1>
-      <p class="card-actions justify-end text-lg">Est: X</p>
+      <p class="card-actions justify-end">Est: X</p>
     </div></span
   >
 </button>
@@ -32,5 +31,29 @@
 <style>
   .isCompleted {
     text-decoration: line-through;
+  }
+  .element {
+    background-image: linear-gradient(
+      45deg,
+      hsl(var(--af)) 50%,
+      hsl(var(--a)) 50%
+    );
+    background-size: 250% 100%;
+    background-position: right bottom;
+    transition: background-position 0.3s ease;
+  }
+  .element:hover {
+    background-position: left top;
+  }
+
+  .isCompletedElement {
+    background-image: linear-gradient(
+      45deg,
+      hsl(var(--af)) 50%,
+      hsl(var(--b1)) 50%
+    );
+    background-size: 250% 100%;
+    background-position: right bottom;
+    transition: background-position 0.3s ease;
   }
 </style>
