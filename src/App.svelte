5699c5b92bbe121a969e3ae1c6f6b41d2991160f
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-27 13:34:48 +0200

    login

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
+  import type { AuthSession } from "@supabase/supabase-js"
+  import { onMount } from "svelte"
+  import Account from "./components/Account.svelte"
+  import Auth from "./components/Auth.svelte"
   import ListView from "./components/ListView.svelte"
   import Navbar from "./components/Navbar.svelte"
+  import { supabase } from "./lib/supabaseClient"
+
+  let session: AuthSession
+
+  onMount(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      if (data.session) {
+        session = data.session
+      }
+    })
+
+    supabase.auth.onAuthStateChange((_event, _session) => {
+      if (_session) {
+        session = _session
+      }
+    })
+  })
 </script>
 
-<div>
-  <Navbar />
-  <ListView />
-</div>
+{#if !session}
+  <Auth />
+{:else}
+  <div>
+    <Account {session} />
+    <Navbar />
+    <ListView />
+  </div>
+{/if}
