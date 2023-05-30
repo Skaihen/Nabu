0e4d4fee15f8420ffdba367e7bf8961da6503e1f
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-05-30 17:34:14 +0200

    Update Auth.svelte

--- src/components/Auth/Auth.svelte
+++ src/components/Auth/Auth.svelte
@@ -1,74 +1,3 @@
-<!-- <script lang="ts">
-  import { supabase } from "../../lib/supabaseClient"
-  import EmailConfirm from "./EmailConfirm.svelte"
-  import EmailDeny from "./EmailDeny.svelte"
-
-  let loading = false
-  let email = ""
-  let hideEmailDeny = true
-  let hideEmailConfirm = true
-
-  const handleLogin = async () => {
-    try {
-      loading = true
-      const { error } = await supabase.auth.signInWithOtp({ email })
-      if (error) throw error
-      hideEmailConfirm = false
-      setTimeout(() => {
-        hideEmailConfirm = true
-      }, 4000)
-    } catch (error) {
-      if (error instanceof Error) {
-        hideEmailDeny = false
-        setTimeout(() => {
-          hideEmailDeny = true
-        }, 4000)
-      }
-    } finally {
-      loading = false
-    }
-  }
-</script>
-    <form
-      on:submit|preventDefault={handleLogin}
-      class="mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
-    >
-      <p class="text-center text-lg font-medium">
-        Sign in via magic link with your email below
-      </p>
-
-      <div>
-        <label for="email" class="sr-only">Email</label>
-
-        <div class="relative">
-          <input
-            name="email"
-            type="email"
-            class="input input-bordered w-full p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter email"
-            autocomplete="on"
-            bind:value={email}
-          />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <ion-icon name="at-outline" class="pointer-events-none w-4 h-4" />
-          </span>
-        </div>
-      </div>
-
-      <button
-        type="submit"
-        class="btn btn-primary btn-block"
-        aria-live="polite"
-        disabled={loading}
-      >
-        {loading ? "Loading" : "Send magic link"}
-      </button>
-    </form>
-  </div>
-</div>
- -->
-
 <script lang="ts">
   import type { Provider } from "@supabase/supabase-js"
   import { supabase } from "../../lib/supabaseClient"
@@ -80,24 +9,33 @@
 
   let email: string = ""
   let password: string = ""
+  let loading: boolean = false
   let helperText: HelperText = { error: false, text: null }
 
   const handleLogin = async (type: string) => {
-    const {
-      data: { user },
-      error
-    } =
-      type === "LOGIN"
-        ? await supabase.auth.signInWithPassword({ email, password })
-        : await supabase.auth.signUp({ email, password })
-
-    if (error) {
-      helperText = { error: true, text: error.message }
-    } else if (!user && !error) {
-      helperText = {
-        error: false,
-        text: "An email has been sent to you for verification!"
+    try {
+      loading = true
+      const {
+        data: { user },
+        error
+      } =
+        type === "LOGIN"
+          ? await supabase.auth.signInWithPassword({ email, password })
+          : await supabase.auth.signUp({ email, password })
+      if (error) {
+        throw error
+      } else if (user && !error) {
+        helperText = {
+          error: false,
+          text: "An email has been sent to you for verification!"
+        }
       }
+    } catch (error) {
+      if (error instanceof Error) {
+        helperText = { error: true, text: error.message }
+      }
+    } finally {
+      loading = false
     }
   }
 
@@ -109,9 +47,9 @@
   }
 </script>
 
-<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+<div class="mx-auto max-w-screen-xl px-4 py-9 sm:px-6 lg:px-8">
   <div class="mx-auto max-w-lg">
-    <h1 class="text-center text-2xl font-bold text-primary sm:text-3xl">
+    <h1 class="text-center text-3xl font-bold text-primary">
       Get started with Nabu
     </h1>
 
@@ -120,10 +58,8 @@
       whatever you need!!
     </p>
 
-    <form
-      action=""
-      class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
-    >
+    <!-- on:submit|preventDefault={handleLogin} -->
+    <div class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
       <p class="text-center text-lg font-medium">Sign in to your account</p>
 
       <div>
@@ -135,7 +71,7 @@
             id="email"
             type="email"
             name="email"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            class="input input-bordered w-full p-4 pe-12 text-sm shadow-sm"
             placeholder="Enter email"
             required
           />
@@ -158,7 +94,7 @@
             id="password"
             type="password"
             name="password"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            class="input input-bordered w-full p-4 pe-12 text-sm shadow-sm"
             placeholder="Enter password"
             required
           />
@@ -172,72 +108,76 @@
         </div>
       </div>
 
-      {#if !!helperText.text}
+      {#if helperText.text}
         <div
-          class="border px-1 py-2 my-2 text-center text-sm {helperText.error
+          class="border px-1 py-2 my-2 text-center text-sm rounded-lg shadow-sm {helperText.error
             ? 'bg-red-100 border-red-300 text-red-400'
             : 'bg-green-100 border-green-300 text-green-500'}"
         >
-          {helperText.text}
+          <div class="flex items-center">
+            <ion-icon
+              name={helperText.error
+                ? "close-circle-outline"
+                : "checkmark-circle-outline"}
+              class="stroke-current flex-shrink-0 h-5 w-5"
+            />
+            <span>
+              {helperText.text}
+            </span>
+          </div>
         </div>
       {/if}
 
       <div class="mt-2 flex gap-3">
         <span class="block w-full">
           <button
-            type="submit"
             on:click={() => handleLogin("REGISTER")}
-            class="btn btn-block btn-primary btn-outline shadow btn-sm"
+            type="submit"
+            class="btn btn-block btn-primary btn-outline shadow-sm btn-sm"
+            aria-live="polite"
+            disabled={loading}
           >
-            Sign Up
+            {loading ? "Loading" : "Sign Up"}
           </button>
         </span>
         <span class="block w-full">
           <button
             on:click={() => handleLogin("LOGIN")}
             type="submit"
-            class="btn btn-block btn-primary shadow btn-sm"
+            class="btn btn-block btn-primary shadow-sm btn-sm"
+            aria-live="polite"
+            disabled={loading}
           >
-            Sign In
+            {loading ? "Loading" : "Sign In"}
           </button>
         </span>
       </div>
 
       <div class="mt-3">
-        <div class="relative">
-          <div class="absolute inset-0 flex items-center">
-            <div class="w-full mx-1.5 border-t border-gray-300" />
-          </div>
-          <div class="relative flex justify-center text-sm leading-5">
-            <span class="px-2 bg-white text-gray-500"> Or continue with </span>
-          </div>
+        <div class="divider text-sm text-gray-500">Or continue with</div>
+        <div class="mt-3">
+          <span class="block">
+            <button
+              on:click={() => handleOAuthLogin("github")}
+              type="button"
+              class="w-3/4 mx-auto flex justify-center btn btn-primary shadow-sm btn-sm btn-disabled"
+            >
+              GitHub
+            </button>
+          </span>
         </div>
-
-        <div>
-          <div class="mt-3">
-            <span class="block rounded-md shadow-sm">
-              <button
-                on:click={() => handleOAuthLogin("github")}
-                type="button"
-                class="w-3/4 mx-auto flex justify-center btn btn-primary shadow btn-sm btn-disabled"
-              >
-                GitHub
-              </button>
-            </span>
-          </div>
-          <div class="mt-3">
-            <span class="block rounded-md shadow-sm">
-              <button
-                on:click={() => handleOAuthLogin("google")}
-                type="button"
-                class="w-3/4 mx-auto flex justify-center btn btn-primary shadow btn-sm btn-disabled"
-              >
-                Google
-              </button>
-            </span>
-          </div>
+        <div class="mt-3">
+          <span class="block">
+            <button
+              on:click={() => handleOAuthLogin("google")}
+              type="button"
+              class="w-3/4 mx-auto flex justify-center btn btn-primary shadow-sm btn-sm btn-disabled"
+            >
+              Google
+            </button>
+          </span>
         </div>
       </div>
-    </form>
+    </div>
   </div>
 </div>
